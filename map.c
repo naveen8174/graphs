6ae0b25addc2c01678788df36cb7d03b9326ad6c
naveen8174 <150158418+naveen8174@users.noreply.github.com>
2024-04-18 18:34:21 +0530

    addded structures

--- map.c
+++ map.c
@@ -2,8 +2,29 @@
 #include<stdlib.h>
 struct node{
     char name;
-    char color[10];
+    char color;
     int d;
     struct node* par;
-}a,b,c,d,e,f,g,h,i,j;
+};
+struct adj{
+    char name;
+    struct adj* add;
+};
+struct node* addnode(struct node* p,char a){
+    p=(struct node*)malloc(sizeof(struct node));
+    p->name=a;
+    p->color='w';
+    p->d=0;
+    p->par=NULL;
+}
 
+int main(){
+    struct node* a,*b,*c,*d,*e,*f,*g,*h,*i,*j;
+    struct node* p[10]={a,b,c,d,e,f,g,h,i,j};
+    struct adj* map[10];
+    for(int i=0;i<10;i++){
+        addnode(p[i],97+i);
+    }
+    
+
+}
